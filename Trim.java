fb26f2120f112203951198610bc0a3661cb4017d
Yashasvi <yashasvichilankar27@gmail.com>
2023-04-09 21:25:23 +0530

    Cuts the white space

---
+++ Trim.java
@@ -0,0 +1,12 @@
+class Trim
+{
+public static void main(String args[])
+{
+//trim() cuts the white spaces only from starting and ending of the string;
+String s="         Java Exercise           ";
+System.out.println(s);
+System.out.println(s.trim());
+
+
+}
+}
