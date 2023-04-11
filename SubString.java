128e7d6e52f021187120e05146df4e593ce9cba6
Yashasvi <yashasvichilankar27@gmail.com>
2023-04-11 21:00:26 +0530

    Using substring method

---
+++ SubString.java
@@ -0,0 +1,10 @@
+class SubString
+{
+public static void main(String args[])
+{
+String old="The quick brown fox jumps over the lazy dog.";
+
+System.out.println(old);
+System.out.println(old.substring(10,44));
+}
+}																																															
