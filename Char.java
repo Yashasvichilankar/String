8b2f9ffefba8c9e349df7e52d1b4386b182872d8
Yashasvi <yashasvichilankar27@gmail.com>
2023-04-15 22:14:30 +0530

    Adding file char.java

---
+++ Char.java
@@ -0,0 +1,14 @@
+class Char
+{
+public static void main(String args[])
+{
+String s="Asterisc Computer Institute";
+System.out.println("Original String= "+s);
+
+System.out.println("The character at position 0: "+s.charAt(0));
+System.out.println("The character at position 9: "+s.charAt(9));
+System.out.println("The character at position 18:"+s.charAt(18));
+}
+}
+
+
